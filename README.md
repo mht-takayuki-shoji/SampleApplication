b24d0039f5c6f1e77c4f94d187b3fa67e9ac2c9f
mht-takayuki-shoji <t.shoji@mht-code.com>
2022-06-09 20:21:45 +0900

    ViewBindingの設定

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # SampleApplication
+
+## ViewBinding
+### View Bindingの有効化
+appフォルダ配下の「build.gradle」でView Bindingを有効化する。
+```
+android {
+    // 省略
+    
+    // ViewBindingのセットアップ
+    viewBinding {
+        enabled true
+    }
+}
+```
+
+### FragmentでViewBindingを使用する
+`onCreate`の前に下記を記載する。
+```
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding
+
+}
+```
+```
+override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.monster_detail_fragment, container, false)
+        // 上記を削除し、下記に書き換える
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+```
+```
+override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val alertDialogButton: Button = view.findViewById(R.id.alert_dialog_button)
+        // 上記をViewBindingを利用し書き換えた形が下記になる
+        binding?.alertDialogButton?.setOnClickListener {
+
+        }
+    }
+```
+```
+override fun onDestroy() {
+        super.onDestroy()
+
+        // 下記を追加する
+        _binding = null
+    }
+``` 
+
--- app/src/main/java/com/sampleapplication/MainFragment.kt
+++ app/src/main/java/com/sampleapplication/MainFragment.kt
@@ -1,26 +1,14 @@
 package com.sampleapplication
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat.getSystemService
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.sampleapplication.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -28,15 +16,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var _binding: FragmentMainBinding
+    private val binding get() = _binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
 
     }
@@ -44,17 +31,17 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         var token: String
-        val button = view.findViewById<Button>(R.id.local_push_notification_button)
-        button.setOnClickListener {
+
+        binding.localPushNotificationButton.setOnClickListener {
             Firebase.messaging.token.addOnCompleteListener { task ->
                 if (!task.isSuccessful) {
                     Log.w(TAG, "Fetching FCM registration token failed", task.exception)
@@ -69,23 +56,19 @@ class MainFragment : Fragment() {
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             MainFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
 
